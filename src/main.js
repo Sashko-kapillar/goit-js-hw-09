6767dbd6636a53d937ed4a51e3537264f1b85bab
sashko-kapillar <kapillarua@gmail.com>
2024-09-07 09:10:07 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+const lightbox = new SimpleLightbox('.gallery a');
+
+/* Деякі з найпоширеніших опцій:
+
+captionDelay: Затримка перед відображенням підпису до зображення.
+captionPosition: Позиція підпису (верхня, нижня).
+navText: Текст для кнопок навігації.
+showCounter: Відображати лічильник зображень.  === */
