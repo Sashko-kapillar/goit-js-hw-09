2bae884b0118aabe99fc3f5c5f56b6273925ec35
sashko-kapillar <kapillarua@gmail.com>
2024-09-07 21:14:58 +0300

    create gallery

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-const lightbox = new SimpleLightbox('.gallery a');
-
 /* Деякі з найпоширеніших опцій:
 
 captionDelay: Затримка перед відображенням підпису до зображення.
 captionPosition: Позиція підпису (верхня, нижня).
 navText: Текст для кнопок навігації.
-showCounter: Відображати лічильник зображень.  === */
+showCounter: Відображати лічильник зображень. 
+
+=== */
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const lightbox = new SimpleLightbox('.gallery a');
