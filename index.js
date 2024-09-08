b21f9511e17ec1aa9e7f88e68fbdd6e18c1f0f6a
Sashko-kapillar <168802678+Sashko-kapillar@users.noreply.github.com>
2024-09-08 20:54:38 +0000

    Deploying to gh-pages from @ Sashko-kapillar/goit-js-hw-09@2ab84360e522b2055cf1af02ac67e4d112990c7b 🚀

---
+++ index.js
@@ -0,0 +1,2 @@
+import"./assets/1-gallery-C4sewlt0.js";/* empty css                      */import"./assets/vendor-CgTBfC_f.js";const t=document.querySelector(".feedback-form input");t.addEventListener("input",()=>{t.value.trim()});localStorage.setItem("feedback-form-state","light");console.log(localStorage);
+//# sourceMappingURL=index.js.map
