5809953e3ffa95547d550ca00032b0c3e68f04e4
sashko-kapillar <kapillarua@gmail.com>
2024-09-10 00:30:27 +0300

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,40 @@
 //   пошук форми
 
-const inputForm = document.querySelector('.feedback-form');
-inputForm.addEventListener('input', () => {
-  //  очистити від пробілів
-  inputForm.value.trim();
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+const storedData = localStorage.getItem('feedback-form-state');
+
+if (storedData) {
+  const parsedData = JSON.parse(storedData);
+  formData = parsedData;
+  form.elements.email.value = parsedData.email;
+  form.elements.message.value = parsedData.message;
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  formData[name] = value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 });
 
-const feedbackEmail = localStorage.setItem('feedback-form-state', 'light');
-console.log(localStorage);
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  alert('Форма успішно відправлена!');
+
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+  formData.message = '';
+  form.reset();
+});
